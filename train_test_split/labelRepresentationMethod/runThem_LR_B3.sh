75e4df76a2aeb1d3411975997677dbfeee38e870
Kate Weaver <kweave23@jhu.edu>
2019-06-25 16:30:57 -0400

    batch jobs splitting the 48 combinations

---
+++ train_test_split/labelRepresentationMethod/runThem_LR_B3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SEP=_
+END=.txt
+COC=coverage
+TAPREF=TA
+TOP=1000
+WINDOW=55000
+SLIDE=5000
+for AFFIRM in no yes
+do
+  for NUM in 15 18
+  do
+    for RATIO in 1.35 1.5 2.0
+    do
+      #NEW=/home/kweave23/VISION_train_test_split/labelRepresentation/75K_5K_{$COC}_{$AFFIRM}_{$TAPREF}_{$NUM}_{$RATIO}_{$TOP}
+      NEW=/home/kweave23/VISION_train_test_split/labelRepresentation/55K_5K_$COC$SEP$AFFIRM$SEP$TAPREF$SEP$NUM$SEP$RATIO$SEP$TOP
+      mkdir $NEW
+      cd $NEW
+      python ./../labelRepresentation.py --window $WINDOW --slide $SLIDE --COC $COC --countQ $AFFIRM --TApref $TAPREF --numCellTypes $NUM --thresholdRatio $RATIO --thresholdTop $TOP
+      awk '{print $1}' potentialWindows_params_55K_5K_$COC$SEP$AFFIRM$SEP$TAPREF$SEP$NUM$SEP$RATIO$SEP$TOP$END | sort | uniq -c > chrom.txt
+      echo finished with {$COC}_{$AFFIRM}_{$TAPREF}_{$NUM}_{$RATIO}
+    done
+  done
+done
