fc3e5b516b581b14d48600b12987e89407691bcf
Kate Weaver <kweave23@jhu.edu>
2019-06-25 14:24:59 -0400

    added 3rd window/slide combo

--- data/IDEAS/intersectWindows.sh
+++ data/IDEAS/intersectWindows.sh
@@ -5,11 +5,13 @@
 #PATH2=/Users/kateweaver/taylorLab/VISION/data/IDEAS/only2/ideasVisionV20p8Seg*.bed.o2.bed
 
 PATH1P1=/project/vision/Data/IDEAS/ideasVisionV20p8Seg
-PATH1P2=.bed
+PATH1P2=bed
 WINDOWS1=/project/vision/Data/IDEAS/windows_100K_10K.bed
 WINDOWS2=/project/vision/Data/IDEAS/windows_75000_5000.bed
+WINDOWS3=/project/vision/Data/IDEAS/windows_55000_5000.bed
 WINDOWTYPE1=100K_10K
 WINDOWTYPE2=75K_5K
+WINDOWTYPE3=55K_5K
 
 function bedtools_it {
   bedtools intersect -wa -wb -a $1 -b $2 > $3.window.$4.bed
@@ -18,9 +20,10 @@ function bedtools_it {
 
 for TYPE in B Cd4 Cd8 Cfue Cfum Clp Cmp Er4 Eryad Eryfl G1e Gmp Hpc7 Imk Lsk Mep Mk Mon Neu Nk
 do
-  FILEOI=$PATH1P1$TYPE$PATH1P2
-  bedtools_it $WINDOWS1 $FILEOI $PATH1P1$TYPE $WINDOWTYPE1
-  bedtools_it $WINDOWS2 $FILEOI $PATH1P1$TYPE $WINDOWTYPE2
+  FILEOI=$PATH1P1$TYPE.$PATH1P2
+  #bedtools_it $WINDOWS1 $FILEOI $PATH1P1$TYPE $WINDOWTYPE1
+  #bedtools_it $WINDOWS2 $FILEOI $PATH1P1$TYPE $WINDOWTYPE2
+  bedtools_it $WINDOWS3 $FILEOI $PATHP1$TYPE $WINDOWTYPE3
 done
 
 #for FILE1 in $PATH1
