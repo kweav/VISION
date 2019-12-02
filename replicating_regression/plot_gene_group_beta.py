425dff9607e1619836471e8c799d22be1172b051
Kate Weaver <kweave23@jhu.edu>
2019-12-02 14:25:33 -0500

    subtraction step

--- replicating_regression/plot_gene_group_beta.py
+++ replicating_regression/plot_gene_group_beta.py
@@ -6,7 +6,6 @@ import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 
 tss,dist = sys.argv[1:]
 df_tss = pd.read_csv(tss, sep="\t")
@@ -21,12 +20,18 @@ df_concat = pd.concat([df_tss, df_dist], axis=1, sort=False)
 
 reorder = [7,4,3,25,2,16,0,1,14,20,13,11,24,23,6,15,12,21,26,10,19,8,18,5,22,9,17]
 df_concat_reindex = df_concat.reindex(reorder)
-#print(df_concat_reindex)
+print(df_concat_reindex)
 
-ax = sns.heatmap(df_concat_reindex, vmin=-40, vmax=40, linewidths=0.25, linecolor='gray', cmap='bwr', yticklabels=1, square=True)
+for i in range(8):
+    scalar = df_concat_reindex.iloc[6,i]
+    df_concat_reindex.iloc[:,i] -= scalar
+
+print(df_concat_reindex)
+
+ax = sns.heatmap(df_concat_reindex, vmin=-40, vmax=40, linewidths=0.25, linecolor='gray', cmap='bwr', yticklabels=1, square=True)#, center=df_concat_reindex.iloc[6,0])
 ax.set_yticklabels(list(df_concat_reindex.index), rotation=0)
 ax.yaxis.tick_right()
 ax.set_xticklabels([x.replace('_gene_c','') for x in df_concat_reindex.columns.values], rotation=270)
 plt.tight_layout()
 fig = ax.get_figure()
-fig.savefig('beta_heatmap.png')
+fig.savefig('beta_heatmap_sub0.png')
