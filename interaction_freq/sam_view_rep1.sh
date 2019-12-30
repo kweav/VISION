b632734b69a10aeb91ba1fdc079889e277f6c13f
Kate Weaver <kweave23@jhu.edu>
2019-12-30 11:38:23 -0500

    environments

--- interaction_freq/sam_view_rep1.sh
+++ interaction_freq/sam_view_rep1.sh
@@ -5,13 +5,16 @@ FILE_END_B=_bwa.bam
 F=first
 S=sec
 
+conda activate bwa_env
 for NUM in 58 59 60 61 62
 do
   date; time samtools view -f 0x40 -b -o $FILE_START$NUM$F$FILE_END_B $FILE_START$NUM$FILE_END_B
   date; time samtools view -f 0x80 -b -o $FILE_START$NUM$S$FILE_END_B $FILE_START$NUM$FILE_END_B
   echo $NUM complete
 done
+conda deactivate
 
+conda activate hifive_env
 date; time hifive hic-complete express -B mm10_DpnII_frag.bed --re DpnII --genome mm10 -d -i 0 \
 -S $FILE_START{58}$F$FILE_END_B $FILE_START{58}$S$FILE_END_B \
 -S $FILE_START{59}$F$FILE_END_B $FILE_START{59}$S$FILE_END_B \
@@ -19,3 +22,4 @@ date; time hifive hic-complete express -B mm10_DpnII_frag.bed --re DpnII --genom
 -S $FILE_START{61}$F$FILE_END_B $FILE_START{61}$S$FILE_END_B \
 -S $FILE_START{62}$F$FILE_END_B $FILE_START{62}$S$FILE_END_B \
 -P G1e_rep1_
+conda deactivate
--- interaction_freq/sam_view_rep2.sh
+++ interaction_freq/sam_view_rep2.sh
@@ -5,13 +5,16 @@ FILE_END_B=_bwa.bam
 F=first
 S=sec
 
+conda activate bwa_env
 for NUM in 73 74 75 76 77
 do
   date; time samtools view -f 0x40 -b -o $FILE_START$NUM$F$FILE_END_B $FILE_START$NUM$FILE_END_B
   date; time samtools view -f 0x80 -b -o $FILE_START$NUM$S$FILE_END_B $FILE_START$NUM$FILE_END_B
   echo $NUM complete
 done
+conda deactivate
 
+conda deactivate hifive_env
 date; time hifive hic-complete express -B mm10_DpnII_frag.bed --re DpnII --genome mm10 -d -i 0 \
 -S $FILE_START{73}$F$FILE_END_B $FILE_START{73}$S$FILE_END_B \
 -S $FILE_START{74}$F$FILE_END_B $FILE_START{74}$S$FILE_END_B \
@@ -19,3 +22,4 @@ date; time hifive hic-complete express -B mm10_DpnII_frag.bed --re DpnII --genom
 -S $FILE_START{76}$F$FILE_END_B $FILE_START{76}$S$FILE_END_B \
 -S $FILE_START{77}$F$FILE_END_B $FILE_START{77}$S$FILE_END_B \
 -P G1e_rep2_
+conda deactivate
