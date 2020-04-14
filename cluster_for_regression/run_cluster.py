b369eb2d786dba8097e67f6c6b218ac61d8bb98e
Kate Weaver <kweave23@jhu.edu>
2020-04-14 13:10:38 -0400

    class name

--- cluster_for_regression/run_cluster.py
+++ cluster_for_regression/run_cluster.py
@@ -16,7 +16,7 @@ def main():
     args = parser.parse_args()
     setup_file_locs(args, args.where_run, args.other_path)
     setup_threads(args.threads)
-    model = regress_sampler(args.train_cre, args.train_tss, args.train_exp, args.cre_dist)
+    model = NNJ_cluster(args.train_cre, args.train_tss, args.train_exp, args.cre_dist)
 
     if args.chroms == 'all':
         chrom_list = []
@@ -68,7 +68,7 @@ def setup_file_locs(args, where_run, other_path):
     args.train_tss = argumentToAdd[where_run] + args.train_tss
     args.test_tss = argumentToAdd[where_run] + args.test_tss
 
-class regress_sampler():
+class NNJ_cluster():
     def __init__(self, train_cre, train_tss, train_exp, cre_dist):
         self.cre_dist = cre_dist
         self.exp_values_all, self.cellIndex, self.cell_to_index, self.TSS_chr_all, self.TSSs_all = self.load_expression(train_exp)
