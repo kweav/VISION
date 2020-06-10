e0ff52d2343484fd49e03b520b8f34438b57221f
Kate Weaver <kweave23@jhu.edu>
2020-06-10 14:20:05 -0400

    add thread functionality

--- data/RNAseq/prepare_expression.py
+++ data/RNAseq/prepare_expression.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
-'''Usage: ./prepare_expression.py rnaTPM.txt 41814 TSS_expression.npz'''
+'''Usage: ./prepare_expression.py rnaTPM.txt 41814 TSS_expression.npz
+          ./prepare_expression.py rnaTPM_pc_only.txt 21782 TSS_pc_expression.npz 1'''
 
 import sys
 import numpy as np
+import os
 
 input_file = sys.argv[1]
 tssN = int(sys.argv[2])
+if len(sys.argv) > 4:
+    threads = sys.argv[4]
+else:
+    threads = 1
+
+os.environ["OMP_NUM_THREADS"] = threads
+os.environ["OPENBLAS_NUM_THREADS"] = threads
+os.environ["MKL_NUM_THREADS"] = threads
+os.environ["VECLIB_MAXIMUM_THREADS"] = threads
+os.environ["NUMEXPR_NUM_THREADS"] = threads
+
 
 header_to_index = {"CFU_E_ad": "Cfue",
                    "CFUMK": "Cfum",
